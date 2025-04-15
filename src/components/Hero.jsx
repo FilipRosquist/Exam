c3a8d71de6113bd2c9463909235b15230414e3c7
FilipRosquist <filip.rosquist@gritacademy.se>
2025-04-15 14:11:32 +0200

    1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import Section from "./Section"
 import { curve, heroBackground } from "../assets/"
 import Button from "../components/Button"
 import { PanelHero } from "../assets/"
+import CompanyLogos from "./CompanyLogos"
 
 const Hero = () => {
   return (
@@ -16,9 +17,9 @@ const Hero = () => {
         <div className="relative z-1 max-w-[62rem] mx-auto mt-10 text-center mb-[4rem] md:mb20 lg:mb:[6rem]">
             <h1 className="h1 mb-6">
                 Explore your Possibilities with 
-                <span className="inline-block relative">Pan-El 3G installations{" "} <img src={curve} className="absolute top-full left-0 w-full xl:-mt-2" width={624} height={28} alt="Curve"/></span>
+                <span className="inline-block relative">Pan-El 5G installations{" "} <img src={curve} className="absolute top-full left-0 w-full xl:-mt-2" width={624} height={28} alt="Curve"/></span>
             </h1>
-            <p className="body-1 max-w-3xl mx-auto mb-6 mt-10 text-n-2 lg:mb-8">Pan-El 3G Installations provides expert electrical solutions with a focus on safety, reliability, and quality. Our certified electricians deliver efficient, code-compliant installations for residential, commercial, and industrial needs.</p>
+            <p className="body-1 max-w-3xl mx-auto mb-6 mt-10 text-n-2 lg:mb-8">Pan-El 5G Installations provides expert electrical solutions with a focus on safety, reliability, and quality. Our certified electricians deliver efficient, code-compliant installations for residential, commercial, and industrial needs.</p>
             <Button href="/pricing" white>
                 Get started
             </Button>
@@ -38,11 +39,17 @@ const Hero = () => {
             </div>
 
             <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
-                <img src={heroBackground} width={1440} height={1800} alt="hero" />
-            </div>
-
+            <img
+              src={heroBackground}
+              className="w-full"
+              width={1440}
+              height={1800}
+              alt="hero"
+            />
+          </div>
         </div>
 
+        <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
     </div>
     </Section>
   )
